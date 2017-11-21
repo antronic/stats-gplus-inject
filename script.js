8235e1128ec55771e0bcde75b85dd469c6569438
Jirachai Chansivanon <antronic.inc@gmail.com>
2017-11-21 16:26:42 +0700

    Support comment

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-var AUTHOR_CLASSNAME = '.dzuq1e';
+var AUTHOR_CLASSNAME = 'dzuq1e';
 var AUTHOR_NAME_CLASSNAME = 'nMlfCf';
 
 var YOUR_NAME = 'Jirachai Chansivanon';
 var YOUR_PROFILE_IMG = 'https://lh3.googleusercontent.com/-bXMJ22mKdL0/AAAAAAAAAAI/AAAAAAAAAAA/AFiYof3_tRXMtm69uV3gvUXGo7MzY21Ztg/s32-c-mo/photo.jpg';
 
+var COMMENT_ENABLE = false;
+
+var COMMENT_CLASSNAME = 'OnSI9b';
+var COMMNET_AUTHOR_CLASSNAME = 'PGAhe';
+
 function startInject() {
-  document.querySelectorAll(AUTHOR_CLASSNAME).forEach((e) => installEventListener(e));
+  document.querySelectorAll(AUTHOR_CLASSNAME).forEach(function(e) {iE(e, 'A')});
+  if (COMMENT_ENABLE) {
+    document.querySelectorAll(COMMENT_CLASSNAME).forEach(function(e) {iE(e, 'C')});
+  }
 }
 
-function installEventListener(el) {
-	el.addEventListener('click', () => {
-		changeContent(el);
+function iEt(el, c) {
+	el.addEventListener('click', function() {
+		cC(el);
   });
 }
 
-function changeContent(el) {
-	el.childNodes.forEach((ele) => {
+function cC(el) {
+	el.childNodes.forEach(function (ele) {
 		if (ele.nodeName === 'A') {
 			ele.firstChild.src = YOUR_PROFILE_IMG;
     }
 		if (ele.className === AUTHOR_NAME_CLASSNAME) {
 			ele.firstChild.firstChild.firstChild.innerHTML = YOUR_NAME;
     }
+		if (ele.className === COMMNET_AUTHOR_CLASSNAME) {
+			ele.firstChild.firstChild.innerHTML = YOUR_NAME;
+    }
   });
 }
 
-function collectionInfomation() {
-  AUTHOR_CLASSNAME = prompt('AUTHOR_CLASSNAME', AUTHOR_CLASSNAME);
-  AUTHOR_NAME_CLASSNAME = prompt('AUTHOR_NAME_CLASSNAME', AUTHOR_NAME_CLASSNAME);
+function collectInformation() {
+  AUTHOR_CLASSNAME = '.' + prompt('AUTHOR_CLASSNAME', AUTHOR_CLASSNAME) || AUTHOR_CLASSNAME;
+  AUTHOR_NAME_CLASSNAME = prompt('AUTHOR_NAME_CLASSNAME', AUTHOR_NAME_CLASSNAME) || AUTHOR_NAME_CLASSNAME;
 
-  YOUR_NAME = prompt('YOUR_NAME', YOUR_NAME);
-  YOUR_PROFILE_IMG = prompt('YOUR_PROFILE_IMG', YOUR_PROFILE_IMG);
+  YOUR_NAME = prompt('YOUR_NAME', YOUR_NAME) || YOUR_NAME;
+  YOUR_PROFILE_IMG = prompt('YOUR_PROFILE_IMG', YOUR_PROFILE_IMG) || YOUR_PROFILE_IMG;
+
+  COMMENT_ENABLE = confirm('COMMENT_ENABLE');
+
+  if (COMMENT_ENABLE) {
+    COMMENT_CLASSNAME = '.' + prompt('COMMENT_CLASSNAME', COMMENT_CLASSNAME) || COMMENT_CLASSNAME;
+    AUTHOR_NAME_CLASSNAME = prompt('AUTHOR_NAME_CLASSNAME', AUTHOR_NAME_CLASSNAME);
+  }
   startInject();
 }
 
 (function() {
-  collectionInfomation();
+  collectInformation();
 })();
